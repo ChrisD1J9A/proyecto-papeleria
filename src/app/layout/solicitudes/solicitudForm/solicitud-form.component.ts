585c96b0eb764f884184dccd4342b47911afabbf
ChrisD1J9A <diego-19gyc@hotmail.com>
2022-02-04 16:59:25 -0600

    04/02/2022

--- src/app/layout/solicitudes/solicitudForm/solicitud-form.component.ts
+++ src/app/layout/solicitudes/solicitudForm/solicitud-form.component.ts
@@ -4,6 +4,7 @@ import swal from 'sweetalert2';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Solicitud } from '../solicitud';
+import { SolicitudesService } from '../solicitudes.service';
 import { Detalle_solicitud } from '../detalle_solicitud';
 import { ProductosService } from 'src/app/layout/catalogo/productos/productos.service';
 import { Producto } from 'src/app/layout/catalogo/productos/producto';
@@ -18,15 +19,15 @@ export class SolicitudFormComponent implements OnInit {
   solicitud = new Solicitud();
   date = new FormControl(new Date());
   displayedColumns: string[] = ['id_producto', 'unidad', 'descripcion', 'action'];
-  deta: Detalle_solicitud[];
+  detas: any;
   dataSource = new MatTableDataSource();
   productosSeleccionados = new Set<Producto>();
   producto = new Producto();
 
-  constructor(private productosService: ProductosService, private formBuilder: FormBuilder) { }
+  constructor(private productosService: ProductosService, private formBuilder: FormBuilder, private solicitudesService: SolicitudesService, private router: Router) { }
 
   get detalles(): FormArray {
-    return this.solicitudForm.get('detalles') as FormArray;
+    return this.detalleSForm.get('detalles') as FormArray;
   }
 
   solicitudForm = this.formBuilder.group({
@@ -34,8 +35,11 @@ export class SolicitudFormComponent implements OnInit {
     id_sucursal: [''],
     fecha_solicitud: [new Date()],
     observacion_solicitud: ['Deje aquí sus Comentarios u observaciones a cerca de la solicitud...'],
-    detalles: this.formBuilder.array([])
   });
+
+  detalleSForm = this.formBuilder.group({
+    detalles: this.formBuilder.array([])
+  })
 
   ngOnInit(): void {
     this.productosService.getProductos().subscribe(
@@ -50,35 +54,69 @@ export class SolicitudFormComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.solicitudForm.value);
-    console.log(this.solicitudForm.getRawValue());
+    this.solicitud = this.solicitudForm.value;
+    console.log(this.solicitud);
+    this.detas = this.detalles.value;
+    console.log(this.detas);
+    swal.fire({
+      title: '¿Desea hacer una nueva solicitud? ',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Si',
+      denyButtonText: `No guardar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+      this.solicitudesService.create2(this.solicitud, this.detas)
+      .toPromise()
+      .then((response) => {
+        if (response.solicitud) {
+          swal.fire(
+            'Nueva solicitud hecha',
+            `Solicitud hecha:  ${this.solicitud.fecha_solicitud} espere su aprobación`,
+            'success'
+          );
+          this.router.navigate(['/layout/solicitudes'])
+          this.ngOnInit();
+        } else if (response.mensaje) {
+          swal.fire('Mensaje', `${response.mensaje}!`, 'error');
+        } else {
+          swal.fire('Mensaje', `Error al envíar la solicitud`, 'error');
+        }
+      })
+      .catch((error) => {
+        swal.fire('Error', `Error al envíar la solicitud`, 'error');
+      });
+      } else if (result.isDenied) {
+        swal.fire('La solicitud no fue guardada', '', 'info')
+      }
+    })
   }
 
-  agregarProducto(producto: Producto) {
-    this.productosSeleccionados.add(producto);
-    this.agregarDetalles(producto);
-    this.producto=producto;
-  }
+agregarProducto(producto: Producto) {
+  this.productosSeleccionados.add(producto);
+  this.agregarDetalles(producto);
+  this.producto = producto;
+}
 
-  eliminarProducto(producto: Producto, index: number) {
-    this.productosSeleccionados.delete(producto);
-    this.removerDetalles(index);
-  }
+eliminarProducto(producto: Producto, index: number) {
+  this.productosSeleccionados.delete(producto);
+  this.removerDetalles(index);
+}
 
-  agregarDetalles(p: Producto) {
-    const detalleFormC = this.formBuilder.group({
-      producto_obj: [p],
-      producto: [{value: p.descripcion, disabled: true}],
-      cant_existente: [''],
-      cant_solicitada: ['']
-    });
+agregarDetalles(p: Producto) {
+  const detalleFormC = this.formBuilder.group({
+    producto_obj: [p],
+    producto: [{ value: p.descripcion, disabled: true }],
+    cant_existente: [''],
+    cant_solicitada: ['']
+  });
 
-    this.detalles.push(detalleFormC);
-  }
+  this.detalles.push(detalleFormC);
+}
 
-  removerDetalles(indice: number)
-  {
-    this.detalles.removeAt(indice);
-  }
+removerDetalles(indice: number)
+{
+  this.detalles.removeAt(indice);
+}
 
 }
--- src/app/layout/solicitudes/solicitudes.service.ts
+++ src/app/layout/solicitudes/solicitudes.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Solicitud } from './solicitud';
+import { Detalle_solicitud } from './detalle_solicitud';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,9 +10,21 @@ import { map } from 'rxjs/operators';
 })
 export class SolicitudesService {
   private urlEndPoint: string = 'http://localhost:8080/api/solicitudes';
+  private urlEndPoint2: string = 'http://localhost:8080/api/solicitudesDet';
   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) { }
+
+  public create(solicitud: Solicitud): Observable<Solicitud>
+  {
+      return this.http.post<Solicitud>(this.urlEndPoint, solicitud, {headers: this.httpHeaders})
+  }
+
+  public create2(solicitud: Solicitud, detalles: any): Observable<any>
+  {
+      const params = new HttpParams().set('detalles',detalles);
+      return this.http.post<any>(this.urlEndPoint2, solicitud, {params: params, headers: this.httpHeaders});
+  }
 
 
   getSolicitudes(): Observable<Solicitud[]> {
